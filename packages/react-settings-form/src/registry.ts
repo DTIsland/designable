9894173ce4693e2006523caf1eaff5c6f3dd6dc4
wewoor <wewoor@gmail.com>
2024-10-10 14:44:56 +0800

    fix: reset monaco loader

--- packages/react-settings-form/src/registry.ts
+++ packages/react-settings-form/src/registry.ts
@@ -7,17 +7,17 @@ const Registry = {
 
 export const setNpmCDNRegistry = (registry: string) => {
   Registry.cdn = registry
-  if (registry) {
-    loader.config({
-      paths: {
-        vs: `${registry}/monaco-editor@0.30.1/min/vs`,
-      },
-    })
-  } else {
-    loader.config({
-      monaco,
-    })
-  }
+  loader.config({
+    paths: {
+      vs: `${registry}/monaco-editor@0.30.1/min/vs`,
+    },
+  })
+  // if (registry) {
+  // } else {
+  //   loader.config({
+  //     monaco,
+  //   })
+  // }
 }
 
 export const getNpmCDNRegistry = () => String(Registry.cdn).replace(/\/$/, '')
